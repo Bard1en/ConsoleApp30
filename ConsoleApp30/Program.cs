2c48c05495e0498326d734af70585e4eeabef857
Bard1en <greger.grym@gmail.com>
2024-02-14 10:36:40 +0100

    Add project files.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace fornite
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        
+           {
+           int  tal1 = 2;
+          int   tal2 = 3;
+          int  tal3 = 34;
+            Console.WriteLine(addera(tal1, tal2, tal3));
+
+        }
+            
+            static int addera(int tal1,  int tal2, int tal3)
+            {
+                
+                return tal1 + tal2 + tal3;
+            }
+        
+    }
+}
+
